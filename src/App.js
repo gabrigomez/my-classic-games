ad7692156834592e5df8610dcfda6be479094805
Gabriel <gabrigomez15@gmail.com>
2022-12-10 12:18:08 -0300

    feat: add first components and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import { Login } from './components/Login'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div>
-          First Commit
-        </div>       
+        <Login />      
       </header>
     </div>
   );
