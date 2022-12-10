4343a1a806aaceb438965821197edba08b6dd255
Gabriel <gabrigomez15@gmail.com>
2022-12-10 12:25:34 -0300

    feat: add basic routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Login } from './components/Login'
+import { SignUp } from './components/SignUp';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Login />      
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>      
       </header>
     </div>
   );
