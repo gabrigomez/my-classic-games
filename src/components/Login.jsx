7f1149250923c6f6557f8a2dd0bfd6ae0a66c048
Gabriel <gabrigomez15@gmail.com>
2022-12-11 13:57:25 -0300

    feat: edit login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Login.css'
 
 export const Login = () => {
   return (
-    <div>
-      Login Form Here
+    <div className='login-form-container'>
+      <div className='form'>        
+        <h1>
+          Login
+        </h1>
+        <div className='email'>
+          <input type="text" name='email' placeholder='E-mail' />
+        </div>
+        <div className='password'>
+          <input type="password" name='password' placeholder='Password' />
+        </div>        
+        <button> 
+          Login
+        </button>        
+      </div>
     </div>
   )
 }
