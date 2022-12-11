ea01c22ed8e19dba3b5183834870aeef1d869bcd
Gabriel <gabrigomez15@gmail.com>
2022-12-11 12:49:35 -0300

    feat: add phosphor icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { GameController, PaperPlaneRight, Scroll } from 'phosphor-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -7,17 +8,20 @@ export const NavBar = () => {
   return (
     <nav>      
         <div className='home'>
+        <GameController className='icon' color='white' size={24}  />
           <Link to="/">
             Home
           </Link>
           </div>
         <div className='login-container'>
           <div className='login'>
+          <PaperPlaneRight className='icon' color='white' size={24} />
             <Link to="/login">
               Login
             </Link>
           </div>
           <div className='sign-up'>
+            <Scroll className='icon' color='white' size={24} />
             <Link to="/signup">
               Sign Up
             </Link>
