bcdaf2ccb1e51e3a5d6b8a5eab2776d371b5bacd
Gabriel <gabrigomez15@gmail.com>
2022-12-11 12:26:49 -0300

    feat: add links to navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './NavBar.css';
 
 export const NavBar = () => {
   return (
     <nav>      
-        <div className='home'>Home</div>
+        <div className='home'>
+          <Link to="/">
+            Home
+          </Link>
+          </div>
         <div className='login-container'>
           <div className='login'>
-            Login
+            <Link to="/login">
+              Login
+            </Link>
           </div>
           <div className='sign-up'>
-            Sign Up
+            <Link to="/signup">
+              Sign Up
+            </Link>
           </div>
         </div>      
     </nav>
