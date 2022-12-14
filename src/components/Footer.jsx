5d785dab84774543fdf20c4c8a1379cc0cb0437e
Gabriel <gabrigomez15@gmail.com>
2022-12-14 17:00:56 -0300

    feat: add icon and github link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
+import { FinnTheHuman } from 'phosphor-react';
 import React from 'react';
 import './Footer.css'
 
 export const Footer = () => {
   return (
     <div className='footer-container'>
-      <p> Develop by <a href="">gabrigomez</a> </p>
+      <p>Develop by <a href="https://github.com/gabrigomez">
+        <FinnTheHuman className='footer-icon' size={24} color="black" />
+        gabrigomez
+        </a>
+      </p>
     </div>
   )
 }
