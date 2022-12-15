5e11c968035d184f8b414fe0b2495c12e7e324de
Gabriel <gabrigomez15@gmail.com>
2022-12-15 12:57:04 -0300

    feat: make store available in index.js

--- src/store.js
+++ src/store.js
@@ -1,11 +1,13 @@
-import { configureStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
 const middleware = [thunk];
 
-export const store = configureStore(
+const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(...middleware))
 );
+
+export default store;
