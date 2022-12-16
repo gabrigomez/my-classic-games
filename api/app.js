98b3d3ee41522c8941f6c0b1cef413aa8efc4df8
Gabriel <gabrigomez15@gmail.com>
2022-12-16 10:48:20 -0300

    feat: integrate to mongoDB in cloud

--- api/app.js
+++ api/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: "api/.env" });
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
@@ -7,7 +7,21 @@ const jwt = require('jsonwebtoken');
 const app =  express()
 
 app.get('/', (req, res) => {
-  res.status(200).json({ msg: "Welcome to the my-game-list api"})
-})
+  res.status(200).json({ msg: "Welcome to the my-game-list api"});
+});
 
-app.listen(3001)
+//Credencials
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+const dbLink = `mongodb+srv://${dbUser}:${dbPass}@cluster0.1q4iqd6.mongodb.net/?retryWrites=true&w=majority`
+mongoose.set("strictQuery", false);
+
+console.log(dbUser);
+
+mongoose.connect(dbLink).then(() => {
+  app.listen(3001)
+  console.log('Conected to MongoDB')
+
+}).catch((err) => console.log(err));
+
