49e4b549fdbbbe60617ad2ef6b00b341c9a99f63
Gabriel <gabrigomez15@gmail.com>
2022-12-16 11:20:20 -0300

    feat: add first user validations

--- api/app.js
+++ api/app.js
@@ -5,12 +5,31 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const app =  express()
-
 app.use(express.json())
+const User = require('./models/User')
 
 app.get('/', (req, res) => {
   res.status(200).json({ msg: "Welcome to the my-game-list api"});
 });
+
+//Register User
+app.post('/auth/register', async(req, res) => {
+  
+  const {username, email, password, confirmPassword} = req.body;
+  // Validations
+  if(!username) {
+    return res.status(422).json({ msg: 'O nome de usuário é obrigatório'});
+  }
+  if(!email) {
+    return res.status(422).json({ msg: 'O e-mail de usuário é obrigatório'});
+  }
+  if(!password) {
+    return res.status(422).json({ msg: 'A senha de usuário é obrigatória'});
+  }
+  if(password !== confirmPassword) {
+    return res.status(422).json({ msg: 'As senhas não conferem'});
+  }
+})
 
 //Credencials
 const dbUser = process.env.DB_USER;
