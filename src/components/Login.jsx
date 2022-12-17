749f27adb3211b4d86da28a8cb14cbaea56103f1
Gabriel <gabrigomez15@gmail.com>
2022-12-17 11:25:39 -0300

    fix: rewrite login component without formik and yup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,105 +1,61 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, useNavigate } from 'react-router-dom';
-
+import axios from 'axios';
 import './Login.css'
-import { login } from "../actions/auth";
 
-import { Formik } from "formik";
-import * as Yup from "yup";
 
 export const Login = (props) => {
-  let navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-  const { isLoggedIn } = useSelector(state => state.auth);
-  const { message } = useSelector(state => state.message);
-
-  const dispatch = useDispatch();
-
-  if (isLoggedIn) {
-    return <Navigate to="/" />
-  } 
-
-  const validationSchema = Yup.object().shape({
-    username: Yup.string()
-      .required("Email is required"),
-    password: Yup.string()
-      .required("Passwort is required")
-      .min(6, "Password too short! Must be at least 6 characters."),
-  });
-
-  const handleSubmit = (
-    { username, password },
-    { setFieldError }
-  ) => {    
-    if(!message) {
-      dispatch(login(username, password))
-          .then(() => {
-            navigate("/");
-            console.log(localStorage.getItem('user'));
-          }) 
-      setTimeout(() => {
-        setFieldError("email", "This email is already taken");
-      }, 1000);
-    } else {
-      console.log(message)
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await axios.post("http://localhost:3001/auth/login", {
+          email,
+          password: password,
+        });     
+      if (res.status === 201) {
+        setEmail("");
+        setPassword("");
+        setMessage("Logado com sucesso!");
+        localStorage.setItem('token', res.data.token);
+      }
+    } catch (err) {
+      console.log(err);
+      setMessage(err.response.data.msg)
     }
   };
 
   return (
-    <div className='login-form-container'>
-      <Formik
-        initialValues={{
-          username: "",
-          password: "",          
-        }}
-        onSubmit={(values, errors) => {
-          handleSubmit(values, errors);
-        }}
-        validationSchema={validationSchema}
-      >
-        {({
-            values,
-            errors,
-            touched,
-            handleSubmit,
-            handleChange,
-            handleBlur,
-          }) => {
-          return (
-            <form className='login-form' onSubmit={handleSubmit}>        
-              <h1>
-                Login
-              </h1>
-              <div className='email'>
-                <input 
-                  type="text" 
-                  name='username' 
-                  placeholder='Username'
-                  value={values.username}
-                  onChange={handleChange("username")}
-                  onBlur={handleBlur("username")}                  
-                />
-                <div>{touched.username && errors.username}</div>
-              </div>
-              <div className='password'>
-                <input 
-                  type="password" 
-                  name='password' 
-                  placeholder='Password'
-                  value={values.password}
-                  onChange={handleChange("password")}
-                  onBlur={handleBlur("password")}                  
-                />
-                <div>{touched.password && errors.password}</div>
-              </div>        
-              <button className='login-button' type='submit'> 
-                Login
-              </button>
-            </form>
-          )}
-        }        
-      </Formik>
+    <div className='login-form-container'>      
+      <form className='login-form' onSubmit={handleSubmit}>        
+        <h1>
+          Login
+        </h1>
+        <div className='email'>
+          <input 
+            type="text" 
+            name='email' 
+            placeholder='E-mail'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+        </div>
+        <div className='password'>
+          <input 
+            type="password" 
+            name='password' 
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>        
+        <button className='login-button' type='submit'> 
+          Login
+        </button>
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
     </div>
   )
 }
