67e539731f4e5978647a3e5452f5e5d83c2cc317
Gabriel <gabrigomez15@gmail.com>
2022-12-20 11:21:39 -0300

    fix: login component with useEffect to clean the form errors

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,39 @@
-import React, { useState } from 'react';
-import './Login.css'
+import React, { useEffect, useState } from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import toast, { Toaster } from 'react-hot-toast';
+import { closeError, login } from "../actions/auth";
 
-
-import { login } from "../actions/auth";
+import './Login.css'
 
 export const Login = () => {
+  const { message } = useSelector(state => state.message);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();   
+  
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { isLoggedIn } = useSelector(state => state.auth);
-  const { message } = useSelector(state => state.message);
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();   
 
-  const handleSubmit = (e) => {
+  let handleSubmit = (e) => {
     e.preventDefault();
 
     dispatch(login(email, password))
       .then(() => {
         navigate("/dashboard");        
       });
-    if(message) {
-      toast(`${message}`, {
-        duration: 3000,
-        style: {
-          color: 'red',
-          background: 'none',
-          border: 'rgb(3, 235, 243)',
-          fontWeight: 'bold'
-        }
-      })
-    }
-  };
- 
+  }; 
+  
+  useEffect(() => {
+    dispatch(closeError());
+  }, [dispatch]);
+
   if (isLoggedIn) {
     return <Navigate to="/dashboard" />;
-  } 
+  }   
 
   return (
-    <div className='login-form-container'>  
-      <Toaster />    
+    <div className='login-form-container'>      
       <form className='login-form' onSubmit={handleSubmit}>        
         <h1>
           Login
@@ -65,10 +56,10 @@ export const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>        
-        <button className='login-button' type='submit'> 
+        <button className='login-button'> 
           Login
         </button>
-        {/* <div className="message">{message ? <p>{message}</p> : null}</div> */}
+        {<div className="message">{message ? <p>{message}</p> : null}</div>}
       </form>
     </div>
   )
