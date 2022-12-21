9a9f80deb044263cf858d81b149d5b901041b552
Gabriel <gabrigomez15@gmail.com>
2022-12-21 12:46:09 -0300

    feat: add editUser basic styles and fields

--- src/components/EditUser.jsx
+++ src/components/EditUser.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import './EditUser.css'
 
 export const EditUser = () => {
+  const { isLoggedIn, user: currentUser } = useSelector(state => state.auth);
+
+  if(!isLoggedIn){
+    return <Navigate to="/login" />
+  }
+
   return (
-    <div className='edit-user-container'>
-      Edit user info here
-    </div>
+    <form className='edit-user-container'>
+      <div className='edit-user-card'>
+        <div className='edit-username'>
+          <h1>
+            Username
+          </h1>
+          <input placeholder={currentUser.user.username} />            
+        </div>
+        <div className='edit-user-email'>
+          <h1>
+            Email
+          </h1>
+          <input placeholder={currentUser.user.email} />      
+        </div>
+        <button className='edit-button'>
+          Save Changes
+        </button>
+      </div>
+    </form>
   )
 }
