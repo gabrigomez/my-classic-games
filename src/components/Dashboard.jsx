5eea6d2a55ace6839d740b5c59ec797590c02687
Gabriel <gabrigomez15@gmail.com>
2022-12-21 10:22:17 -0300

    feat: basic fields in user dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,7 +4,8 @@ import { Navigate } from 'react-router-dom';
 import './Dashboard.css';
 
 export const Dashboard = () => {
-  const { isLoggedIn, user: currentUser } = useSelector(state => state.auth);  
+  const { isLoggedIn, user: currentUser } = useSelector(state => state.auth);
+  console.log(currentUser)  
 
   if(!isLoggedIn){
     return <Navigate to="/login" />
@@ -12,12 +13,32 @@ export const Dashboard = () => {
   return (
     <div className='dashboard-container'>
       <div className='dashboard-card'>
-        <h1 className='dashboard-title'>
-          Hey, {currentUser.user.username}
-        </h1>
-        <p>
-          Welcome to your dashboard
-        </p>
+        <div className='user-gamelist'>          
+            <h1 className='dashboard-title'>
+              Hey, {currentUser.user.username}!
+            </h1>          
+          <h3>
+            Welcome to My game list dashboard.
+          </h3>
+          <div className='gamelist'>
+            My game list will be here
+          </div>
+        </div>
+        <div className='user-card'>
+          <div className='user-info'>
+            <div className='user-email'>              
+              <div>
+                {currentUser.user.username}
+              </div>
+              <div>
+                {currentUser.user.email}
+              </div>
+              <a href="">
+                Edit my infos
+              </a>              
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
