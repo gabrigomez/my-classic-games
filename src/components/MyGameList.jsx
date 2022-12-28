b76e2627f1912b43b54dcdc9c5ca411dbff98c97
Gabriel <gabrigomez15@gmail.com>
2022-12-28 12:02:14 -0300

    feat: start myGameList component

--- src/components/MyGameList.jsx
+++ src/components/MyGameList.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import "./MyGameList.css";
 
 export const MyGameList = () => {
   return (
     <div className='my-game-list-container'>
-      My game List
+      <div className='gamelist-card'>
+        <div className='game-card'>
+          game1
+        </div>
+        <div className='game-card'>
+          game2
+        </div>
+        <div className='game-card'>
+          game3
+        </div>
+        <div className='game-card'>
+          game4
+        </div>
+        <div className='game-card'>
+          game5
+        </div>
+        <div className='game-card'>
+          game6
+        </div>
+        <div className='game-card'>
+          game7
+        </div>
+        <div className='game-card'>
+          game8
+        </div>
+      </div>
     </div>
   )
 }
