39a48d87db6a745135ffe3f249eccac8ea51bab4
Gabriel <gabrigomez15@gmail.com>
2023-01-03 10:57:11 -0300

    fix: imageUrl in game model

--- api/models/Game.js
+++ api/models/Game.js
@@ -4,7 +4,7 @@ const Game = mongoose.model('Game', {
   title: String,
   genre: String,
   description: String,
-  image: String,
+  imageUrl: String,
   user_id: {type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
