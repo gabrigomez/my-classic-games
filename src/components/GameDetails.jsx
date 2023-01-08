22d5c4b47b563832d0466097ee2f5c3ebf7c7deb
Gabriel <gabrigomez15@gmail.com>
2023-01-08 10:50:50 -0300

    feat: redirect user when delete a game

--- src/components/GameDetails.jsx
+++ src/components/GameDetails.jsx
@@ -1,6 +1,7 @@
 import { Trash } from 'phosphor-react';
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { clearMessage, deleteGame, showGameDetails } from '../features/users/userSlice';
 import './GameDetails.css';
 
@@ -9,11 +10,14 @@ export const GameDetails = () => {
   const gameId = window.location.href.split('/').reverse()[0];
   
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
 
   const handleDelete = () => {
     dispatch(deleteGame({id: gameId}));
     setTimeout(() => {
       dispatch(clearMessage());
+      navigate('/my-game-list');
     }, 2000);
   }
 
